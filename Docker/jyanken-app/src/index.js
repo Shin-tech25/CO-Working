4e0d204dc33f4c808de2b2dc165978b8a28e2214
Shin_tech25 <m.shin0509@gmail.com>
2022-06-29 00:32:32 +0900

    jyanken-app

--- Docker/jyanken-app/src/index.js
+++ Docker/jyanken-app/src/index.js
@@ -36,13 +36,13 @@ const JyankenBox = (props) => {
   return (
     <div>
       <button onClick={() => props.actionPon(0)}>グー</button>
-      <button onClick={() => props.actionPon(0)}>チョキ</button>
-      <button onClick={() => props.actionPon(0)}>パー</button>
+      <button onClick={() => props.actionPon(1)}>チョキ</button>
+      <button onClick={() => props.actionPon(2)}>パー</button>
     </div>
   )
 }
 
-JyankenBox.PropTypes = {
+JyankenBox.propTypes = {
   actionPon: PropTypes.func
 }
 
