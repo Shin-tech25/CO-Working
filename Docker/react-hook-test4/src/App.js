1deed6075d1089745795d84fc35a7504e96d53a2
Shin_tech25 <m.shin0509@gmail.com>
2022-07-01 08:03:23 +0900

    build

--- Docker/react-hook-test4/src/App.js
+++ Docker/react-hook-test4/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // useEffectの第２引数を指定しないため、コンポーネント（この場合ではAppコンポーネント）がレンダーされた後、毎回実行されている。
+  useEffect(() => {
+    console.log(document.getElementById("effectHook").innerText);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p id="effectHook">You clicked { count } times</p>
+      <button onClick={ ()=> setCount(count+1) }></button>
+    </>
+  )
 }
 
 export default App;
