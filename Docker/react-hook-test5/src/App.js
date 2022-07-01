6dccf82c47054a560e7c330a8a9f4181600065d7
Shin_tech25 <m.shin0509@gmail.com>
2022-07-01 09:50:33 +0900

    外部APIとのデータをやり取りするApp

--- Docker/react-hook-test5/src/App.js
+++ Docker/react-hook-test5/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+// HTTP通信をするモジュール（外部APIなどと通信するときに利用する）
+import axios from 'axios'
 
 function App() {
+  // APIから取得したデータ
+  const [items, setItems] = useState([]);
+  // ローディング状態
+  const [isLoading, setIsLoading] = useState(false);
+
+  // コンポーネントが初めてレンダーされた後に外部APIからデータを取得し、stateを更新する副作用。
+  // useEffectの第2引数（今回は空の配列）を指定しないと、itemsやisLoadingが更新され、コンポーネントが再レンダーされる度にAPIとの通信が発生してしまう。
+  // 今回はコンポーネントがレンダーされた後に、一度だけこの処理を実行したいため、第2引数に[]を渡している。
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=react"
+      );
+
+      setItems(result.data.hits);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        isLoading ? (
+          <p>Loading</p>
+        ) : (
+          <ul>
+            {items.map((item) => (
+              <li key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        )
+      }
+    </>
   );
 }
 
