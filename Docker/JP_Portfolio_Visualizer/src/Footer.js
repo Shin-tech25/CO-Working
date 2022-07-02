391a7e21eb537f253ee722c7dbad20439da73090
Shin_tech25 <m.shin0509@gmail.com>
2022-07-02 21:54:18 +0900

    jp_portfolio_visualizerの雛形作成

---
+++ Docker/JP_Portfolio_Visualizer/src/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <>
+      <div className="Footer">
+        <p>Footer</p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ Docker/JP_Portfolio_Visualizer/src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <>
+      <div className="Header">
+        <p>Header</p>
+      </div>
+    </>
+  );
+}
+
+export default Header;
