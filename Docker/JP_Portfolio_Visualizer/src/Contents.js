a3c21389cad7addb6a6ace88a6a16b2e4559ead0
Shin_tech25 <m.shin0509@gmail.com>
2022-07-02 23:12:46 +0900

    Select-BoxをContentsに追加

--- Docker/JP_Portfolio_Visualizer/src/Contents.js
+++ Docker/JP_Portfolio_Visualizer/src/Contents.js
@@ -1,5 +1,6 @@
 import './Contents.css';
 import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
 
 function Explanation() {
   return (
@@ -11,6 +12,174 @@ function Explanation() {
   )
 }
 
+function TimePeriodBox() {
+  const options = [
+    { value: 'Year-to-Year', label: 'Year-to-Year' },
+    { value: 'Month-to-Month', label: 'Month-to-Month' }
+  ]
+  return (
+    <>
+      <p>TimePeriod</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function StartYearBox() {
+  const options = [];
+  for (let i = 1985; i <= 2022; i++){
+    let obj = {};
+    obj['value'] = i;
+    obj['label'] = i;
+    options.push(obj);
+  }
+  return (
+    <>
+      <p>Start Year</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function EndYearBox() {
+  const options = [];
+  for (let i = 1985; i <= 2022; i++){
+    let obj = {};
+    obj['value'] = i;
+    obj['label'] = i;
+    options.push(obj);
+  }
+  return (
+    <>
+      <p>End Year</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function IncludeYTDBox() {
+  const options = [
+    { value: 'Yes', label: 'Yes' },
+    { value: 'No', label: 'No' }
+  ]
+  return (
+    <>
+      <p>IncludeYTDBox</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function CashFlowsBox() {
+  const options = [
+    { value: 'Contribute Fixed Ammount', label: 'Contribute Fixed Ammount' },
+    { value: 'Withdraw Fixed Ammount', label: 'Withdraw Fixed Ammount' },
+    { value: 'Withdraw Fixed Percentage', label: 'Withdraw Fixed Percentage' },
+  ]
+  return (
+    <>
+      <p>Cashflows</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function RebalancingBox() {
+  const options = [
+    { value: 'No rebalancing', label: 'No rebalancing' },
+    { value: 'Rebalance annualy', label: 'Rebalance annualy' },
+    { value: 'Rebalance semi-annualy', label: 'Rebalance semi-annualy' },
+    { value: 'Rebalance quarterly', label: 'Rebalance quarterly' },
+    { value: 'Rebalance monthly', label: 'Rebalance monthly' },
+    { value: 'Rebalance bands', label: 'Rebalance bands' },
+  ]
+  return (
+    <>
+      <p>Rebalancing</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function LeverageTypeBox() {
+  const options = [
+    { value: 'None', label: 'None' },
+    { value: 'Fixed Debt Ammount', label: 'Fixed Debt Ammount' },
+    { value: 'Fixed Leverage Ratio', label: 'Fixed Leverage Ratio' },
+  ]
+  return (
+    <>
+      <p>LeverageTypeBox</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function ReinvestDividendsBox() {
+  const options = [
+    { value: 'Yes', label: 'Yes' },
+    { value: 'No', label: 'No' }
+  ]
+  return (
+    <>
+      <p>Reinvest Dividends</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function DisplayIncomeBox() {
+  const options = [
+    { value: 'Yes', label: 'Yes' },
+    { value: 'No', label: 'No' }
+  ]
+  return (
+    <>
+      <p>Display Income</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function FactorRegressionBox() {
+  const options = [
+    { value: 'Yes', label: 'Yes' },
+    { value: 'No', label: 'No' }
+  ]
+  return (
+    <>
+      <p>Factor Regression</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function BenchmarkBox() {
+  const options = [
+    { value: 'None', label: 'None' },
+    { value: 'Specify Ticker', label: 'Specify Ticker' },
+    { value: 'Import Benchmark', label: 'Import Benchmark' },
+  ]
+  return (
+    <>
+      <p>Benchmark</p>
+      <Select options={options} />
+    </>
+  );
+}
+
+function PortfolioNamesBox() {
+  const options = [
+    { value: 'Default', label: 'Default' },
+    { value: 'Custom', label: 'Custom' }
+  ]
+  return (
+    <>
+      <p>Portfolio Names</p>
+      <Select options={options} />
+    </>
+  );
+}
 
 function Contents() {
   const [timePeriod, setTimePeriod] = useState("Year-to-Year");
@@ -28,13 +197,28 @@ function Contents() {
   const [portfolioNames, setPortfolioNames] = useState("Default");
 
   // Portfolio Assets 配列を追加すること前提にしてどう実装するか？
-  const [portfolioAssets, setPortfolioAssets] = useState("none");
+  // const [portfolioAssets, setPortfolioAssets] = useState("none");
 
   return (
     <>
-      <Explanation />
       <div className="Contents">
-        <p>Contents</p>
+        <Explanation />
+
+        <TimePeriodBox />
+        <StartYearBox />
+        <EndYearBox />
+        <IncludeYTDBox />
+
+        {/* Initial Ammount */}
+
+        <CashFlowsBox />
+        <RebalancingBox />
+        <LeverageTypeBox />
+        <ReinvestDividendsBox />
+        <DisplayIncomeBox />
+        <FactorRegressionBox />
+        <BenchmarkBox />
+        <PortfolioNamesBox />
       </div>
     </>
   );
