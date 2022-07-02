94e1c46ec3572d3ac6d1877f1682271b6c3dc943
Shin <m.shin0509@gmail.com>
2022-07-02 14:53:45 +0900

    react-hook-test11

---
+++ Docker/react-hook-test11/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+
+// props.countが更新されない限り、再レンダーされない。
+const Child = React.memo(({ count }) => {
+  console.log("render Child");
+  return <p>Child</p>;
+});
+
+function App() {
+  console.log("render App");
+
+  const [timeLeft, setTimeLeft] = useState(100);
+  const timerRef = useRef(null);
+  const timeLeftRef = useRef(timeLeft);
+
+  useEffect(()=>{
+    timeLeftRef.current = timeLeft;
+  }, [timeLeft])
+
+  const tick = () => {
+    if(timeLeftRef.current === 0) {
+      clearInterval(timerRef.current);
+      return;
+    }
+    setTimeLeft(prevTime => prevTime -1);
+  }
+
+  const start = () => {
+    timerRef.current = setInterval(tick, 10);
+  }
+
+  const reset = () => {
+    clearInterval(timerRef.current);
+    setTimeLeft(100);
+  };
+
+  return(
+    <>
+      <button onClick={start}>start</button>
+      <button onClick={reset}>reset</button>
+      <p>App: {timeLeft}</p>
+      <Child />
+    </>
+  );
+}
+
+export default App;
