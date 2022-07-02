ffeeb06a6bc53e90a88d21bac58d789fc10c7661
Shin_tech25 <m.shin0509@gmail.com>
2022-07-02 21:10:43 +0900

    react-hook-test15

--- Docker/react-hook-test14/src/App.js
+++ Docker/react-hook-test14/src/App.js
@@ -7,7 +7,8 @@ const App = () => {
   const [count, setCount] = useState(0);
   useEffect(() => {
     document.title = `${count}回クリックされました`
-  })
+    // 依存配列には、countを指定する。空配列だと最初の描画後の一度だけuseEffectで指定した関数が実行される為、うまくいかない。
+  }, [count])
 
   return (
     <>
