86cab113aeddf0355f07845b833e25770dcf9ab4
Shin <m.shin0509@gmail.com>
2022-07-02 10:53:13 +0900

    プルダウンメニューの追加

--- Docker/Docs/.vuepress/config.js
+++ Docker/Docs/.vuepress/config.js
@@ -13,6 +13,12 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'FrontPage', link: '/front/' },
       { text: 'About me', link: 'http://Shin-tech25.com/' },
+      { text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]}
     ],
     sidebar: 'auto'
   }
