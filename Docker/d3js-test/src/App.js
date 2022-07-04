fcb850b9e6f069f636f8fb2d101c1360367ca5a5
Shin_tech25 <m.shin0509@gmail.com>
2022-07-04 21:17:22 +0900

    d3js-test

---
+++ Docker/d3js-test/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import * as d3 from "d3";
+
+// 作成したPieグラフ
+import Pie from "./Pie";
+
+function App() {
+
+  const [width, height, innerRadius, outerRadius] = [200, 200, 60, 100];
+
+  // ボタンをクリックした時、ランダムなデータを生成する
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((_item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+  const [data, setData] = useState(generateData());
+  const changeData = () => {
+    setData(generateData());
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={changeData}>Click</button>
+      </div>
+      <div>
+        <h2 className="label">React Hook</h2>
+        <Pie
+          data={data}
+          width={width}
+          height={height}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
