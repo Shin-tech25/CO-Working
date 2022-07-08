c86df4462dc36d6d88907f6d540257d0c4e0da3c
Shin_tech25 <m.shin0509@gmail.com>
2022-07-08 22:31:39 +0900

    modified:   Docker/used3-hook2/src/index.js

--- Docker/used3-hook2/src/index.js
+++ Docker/used3-hook2/src/index.js
@@ -40,42 +40,5 @@ const App = () => {
   );
 };
 
-// class App extends React.Component {
-//   state = {
-//     width: 300,
-//     height: 300,
-//     data: d3.range(100).map(_ => [Math.random(), Math.random()])
-//   };
-
-//   onClick = () => {
-//     const { width, height } = this.state;
-//     this.setState({
-//       width: width * 0.7,
-//       height: height * 0.7
-//     });
-//   };
-
-//   render() {
-//     const { width, height, data } = this.state;
-
-//     return (
-//       <div className="App">
-//         <h1>Hello CodeSandbox</h1>
-//         <h2>Start editing to see some magic happen!</h2>
-//         <svg width="800" height="800" onClick={this.onClick}>
-//           <Scatterplot
-//             x={50}
-//             y={50}
-//             width={width}
-//             height={height}
-//             data={data}
-//             datapoint={({ x, y }) => <Datapoint x={x} y={y} />}
-//           />
-//         </svg>
-//       </div>
-//     );
-//   }
-// }
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
